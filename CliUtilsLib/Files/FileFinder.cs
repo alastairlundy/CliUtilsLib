19e8eb11c6f195c7f964b597edf4f2569863168d
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-10-17 11:12:53 +0100

    deprecate FileFinder

--- CliUtilsLib/Files/FileFinder.cs
+++ CliUtilsLib/Files/FileFinder.cs
@@ -1,19 +1,21 @@
 ﻿/*
-    Copyright 2024 Alastair Lundy
+    CliUtilsLib
+    Copyright (C) 2024  Alastair Lundy
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, version 3 of the License.
 
-       http://www.apache.org/licenses/LICENSE-2.0
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+using System;
 using System.IO;
 
 namespace CliUtilsLib;
@@ -21,6 +23,7 @@ namespace CliUtilsLib;
 /// <summary>
 /// 
 /// </summary>
+[Obsolete("Use IOExtensions FileFinder instead.")]
 public static class FileFinder
 {
     /// <summary>
@@ -28,6 +31,7 @@ public static class FileFinder
     /// </summary>
     /// <param name="arg">The string to be searched.</param>
     /// <returns>true if the string is a file; return false otherwise.</returns>
+    [Obsolete("Use IOExtensions FileFinder instead.")]
     public static bool IsAFile(string arg)
     {
         try
